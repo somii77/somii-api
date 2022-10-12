a0adb3d1d1262a7a31fc50aa3c8dbdd22733d4ef
Server <server@server.com>
2022-10-12 10:47:54 +0000

    Updated server conf

---
+++ config/env/production/database.js
@@ -0,0 +1,16 @@
+module.exports = ({ env }) => ({
+    connection: {
+      client: 'postgres',
+      connection: {
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'somii-api'),
+        user: env('DATABASE_USERNAME', 'somii-api'),
+        password: env('DATABASE_PASSWORD', 'vnzxYjUBBGDI'),
+        ssl: {
+          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+        },
+      },
+      debug: false,
+    },
+  });
